34352de22433f8c320d9fa70fca986068ba9eba5
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-03-08 23:41:32 +0300

    настроила сборку sass

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var gulp = require( 'gulp' ),
+    sass = require( 'gulp-sass' ),
+    autopref = require( 'gulp-autoprefixer' ),
+    concat = require( 'gulp-concat' );
+
+gulp.task( 'sass', function() {
+    return gulp.src( 'src/**/**.sass' )
+        .pipe(sass())
+        .pipe(autopref({
+            browsers: ['last 3 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest( 'public' ))
+} );
+
+gulp.task( 'watch', ['sass'], function() {
+    gulp.watch( 'src/*/*.*', ['sass'] );
+});
+
+gulp.task('default', ['watch']);
